c1a0e6bd0551a73cc227193f211423c7c9bfa652
Aaron Smith <aaroncolesmith@gmail.com>
2026-03-15 12:56:59 -0700

    Add safe player_id backfill migration

---
+++ supabase_migrations/20260315_backfill_player_id_safe.sql
@@ -0,0 +1,24 @@
+-- One-shot backfill of player_id with safety for missing column
+
+ALTER TABLE public.player_stats
+ADD COLUMN IF NOT EXISTS player_id TEXT;
+
+UPDATE public.player_stats
+SET player_id = CASE player_name
+    WHEN 'Aaron' THEN 'user_aaron'
+    WHEN 'Polina' THEN 'user_polina'
+    WHEN 'Gray-Gray' THEN 'user_gray_gray'
+    WHEN 'Mimi' THEN 'user_mimi'
+    WHEN 'Micah' THEN 'user_micah'
+    WHEN 'Cherrie' THEN 'user_cherrie'
+    WHEN 'Peter-Playwright' THEN 'user_peter_playwright'
+    WHEN 'TEST' THEN 'user_test'
+    WHEN 'Fizz' THEN 'bot_fizz'
+    WHEN 'J-Bock' THEN 'bot_j_bock'
+    WHEN 'Huber' THEN 'bot_huber'
+    WHEN 'Moses' THEN 'bot_moses'
+    WHEN 'Wooden' THEN 'bot_wooden'
+    WHEN 'Buff' THEN 'bot_buff'
+    ELSE player_id
+END
+WHERE player_id IS NULL;
